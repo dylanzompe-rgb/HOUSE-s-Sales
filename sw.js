a92af584c675fe6f1bf62e478e9db5c5236e4d45
dylanzompe-rgb <dylanzompe@gmail.com>
2026-09-24 08:42:18 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='mes-ventes-v1';
+const C='mes-ventes-v2';
 const A=['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
